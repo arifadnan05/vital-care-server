80b134469e16eb5743f540d4039f011fd06e4e4b
arifadnan05 <arifahamedadnan@gmail.com>
2024-06-13 20:31:02 +0600

    manage users added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 
 // middleware
@@ -27,16 +27,44 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db('vitalCare').collection('users')
     const categorySlideCollection = client.db('vitalCare').collection('homeCategory')
+    const medicineCollection = client.db('vitalCare').collection('medicine')
 
-    
+    // user related api
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+  })
+
+    app.post('/users', async (req,res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const exitingUser = await usersCollection.findOne(query)
+      if(exitingUser) {
+          return res.send({message: 'user already exits', insertedId: null})
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+  })
 
 
+
+    // Home category data
     app.get('/categoryCard', async (req, res) => {
       const result = await categorySlideCollection.find().toArray()
       res.send(result)
     })
 
+    // all medicine product get api hare
+    app.get('/medicine', async (req, res) => {
+      const category = req.query.category
+      let query = {}
+      if(category) query = {category}
+      const result = await medicineCollection.find(query).toArray()
+      res.send(result)
+    })
 
 
 
@@ -52,8 +80,8 @@ async function run() {
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send('Vital Care server is running')
+  res.send('Vital Care server is running')
 })
 app.listen(port, () => {
-    console.log(`vital care server is running on port ${port}`)
+  console.log(`vital care server is running on port ${port}`)
 })
