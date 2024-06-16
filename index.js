106e93c39e9b5540aa7cb2abda033867aefafd9b
arifadnan05 <arifahamedadnan@gmail.com>
2024-06-17 00:05:33 +0600

    verify admin function are added

--- index.js
+++ index.js
@@ -55,7 +55,17 @@ async function run() {
       })
 
     }
-
+    // use verify admin after verifyToken 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'Forbidden Access' })
+      }
+      next()
+    }
 
     // user related api
 
