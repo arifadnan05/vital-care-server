ce232cd102d2f6ab9971f057b5c8f48fd57a962b
arifadnan05 <arifahamedadnan@gmail.com>
2024-06-21 01:49:30 +0600

    payment status update are crreated

--- index.js
+++ index.js
@@ -278,6 +278,21 @@ async function run() {
       res.send(result)
     })
 
+
+
+    // payment status update api 
+    app.patch('/payments/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: 'paid'
+        }
+      }
+      const result = await paymentCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
